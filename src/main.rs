3c29335781f9b3f74018b7664f3e70984338f6e9
rhysd <lin90162@yahoo.co.jp>
2019-08-10 22:06:55 +0900

    step2: disable various I/O processings
    - signals: SIGINT, SIGTSTP on Ctrl+C and Ctrl+Z
    - control flow mode on Ctrl+S/Ctrl+Q
    - Ctrl+V
    - CR-to-NL conversion on Ctrl+M
    - output conversion such as \n to \r\n

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
 use std::io::{self, Read};
 use std::os::unix::io::{AsRawFd, RawFd};
-use termios::{self, Termios};
 
 struct InputRawMode {
     fd: RawFd,
-    orig: Termios,
+    orig: termios::Termios,
 }
 
 impl InputRawMode {
     fn new(stdin: &io::Stdin) -> io::Result<InputRawMode> {
+        use termios::*;
+
         let fd = stdin.as_raw_fd();
         let mut termios = Termios::from_fd(fd)?;
         let orig = termios.clone();
 
-        // Set terminal raw mode. Disable echo back and canonical mode.
-        termios.c_lflag &= !(termios::ECHO | termios::ICANON);
-        termios::tcsetattr(fd, termios::TCSAFLUSH, &mut termios)?;
+        // Set terminal raw mode. Disable echo back, canonical mode, signals (SIGINT, SIGTSTP) and Ctrl+V.
+        termios.c_lflag &= !(ECHO | ICANON | ISIG | IEXTEN);
+        // Disable control flow mode (Ctrl+Q/Ctrl+S) and CR-to-NL translation
+        termios.c_iflag &= !(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+        // Disable output processing such as \n to \r\n translation
+        termios.c_oflag &= !OPOST;
+        // Ensure character size is 8bits
+        termios.c_cflag |= CS8;
+        tcsetattr(fd, TCSAFLUSH, &mut termios)?;
 
         Ok(InputRawMode { fd, orig })
     }
@@ -35,7 +42,13 @@ fn main() -> io::Result<()> {
 
     for b in stdin.bytes() {
         let c = b? as char;
-        println!("c: {:?}", c);
+
+        if c.is_control() {
+            print!("{}\r\n", c as i32);
+        } else {
+            print!("{} ({})\r\n", c, c as i32);
+        }
+
         if c == 'q' {
             break;
         }
