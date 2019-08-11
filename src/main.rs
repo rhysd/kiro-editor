7297093f0e95846ec1c2ff21cb547d182914bde2
rhysd <lin90162@yahoo.co.jp>
2019-08-11 20:00:01 +0900

    refactor key and stdin representation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+use std::fmt;
 use std::io::{self, Read};
-use std::os::unix::io::{AsRawFd, RawFd};
+use std::ops::{Deref, DerefMut};
+use std::os::unix::io::AsRawFd;
 
-struct InputRawMode {
-    fd: RawFd,
+struct StdinRawMode {
+    stdin: io::Stdin,
     orig: termios::Termios,
 }
 
-impl InputRawMode {
-    fn new(stdin: &io::Stdin) -> io::Result<InputRawMode> {
+impl StdinRawMode {
+    fn new() -> io::Result<StdinRawMode> {
         use termios::*;
 
+        let stdin = io::stdin();
         let fd = stdin.as_raw_fd();
         let mut termios = Termios::from_fd(fd)?;
         let orig = termios.clone();
@@ -25,38 +28,85 @@ impl InputRawMode {
         // Do not wait for next byte with blocking since reading 0 byte is permitted
         termios.c_cc[VMIN] = 0;
         // Set read timeout to 1/10 second it enables 100ms timeout on read()
-        termios.c_cc[VTIME] = 1;
+        termios.c_cc[VTIME] = 10;
         // Apply terminal configurations
         tcsetattr(fd, TCSAFLUSH, &mut termios)?;
 
-        Ok(InputRawMode { fd, orig })
+        Ok(StdinRawMode { stdin, orig })
     }
 }
 
-impl Drop for InputRawMode {
+impl Drop for StdinRawMode {
     fn drop(&mut self) {
         // Restore original terminal mode
-        termios::tcsetattr(self.fd, termios::TCSAFLUSH, &mut self.orig).unwrap();
+        termios::tcsetattr(self.stdin.as_raw_fd(), termios::TCSAFLUSH, &mut self.orig).unwrap();
+    }
+}
+
+impl Deref for StdinRawMode {
+    type Target = io::Stdin;
+
+    fn deref(&self) -> &Self::Target {
+        &self.stdin
+    }
+}
+
+impl DerefMut for StdinRawMode {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.stdin
+    }
+}
+
+enum SpecialKey {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+#[derive(PartialEq)]
+enum Key {
+    Unidentified,
+    Ascii(u8),
+    // Special(SpecialKey),
+    // TODO: Add Utf8(char),
+}
+
+impl Key {
+    fn ctrl(c: u8) -> Key {
+        Key::Ascii(c & 0x1f)
+    }
+}
+
+impl fmt::Debug for Key {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Key::Unidentified => write!(f, "Key(Unidentified)"),
+            Key::Ascii(code) => {
+                let c = *code as char;
+                if c.is_control() {
+                    write!(f, "Key(0x{:x})", *code)
+                } else {
+                    write!(f, "Key('{}', 0x{:x})", c, *code)
+                }
+            }
+        }
     }
 }
 
 fn main() -> io::Result<()> {
-    let mut stdin = io::stdin();
-    let _raw = InputRawMode::new(&stdin)?;
+    let mut stdin = StdinRawMode::new()?;
     let mut one_byte: [u8; 1] = [0];
 
     loop {
         let size = stdin.read(&mut one_byte)?;
-        debug_assert!(size == 0);
-        let c = if size > 0 { one_byte[0] as char } else { '\0' };
+        debug_assert!(size == 0 || size == 1);
+        let c = if size > 0 { one_byte[0] } else { b'\0' };
+        let k = Key::Ascii(c);
 
-        if c.is_control() {
-            print!("{}\r\n", c as i32);
-        } else {
-            print!("{} ({})\r\n", c, c as i32);
-        }
+        print!("{:?}\r\n", k);
 
-        if c == 'q' {
+        if k == Key::ctrl(b'q') {
             break;
         }
     }
