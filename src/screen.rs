cc38ef169e22323fbad7e9e165d5d6043d4c2494
rhysd <lin90162@yahoo.co.jp>
2019-08-22 14:13:59 +0900

    move constants to file which actually uses them

--- src/screen.rs
+++ src/screen.rs
@@ -7,6 +7,51 @@ use std::io::{self, Write};
 use std::time::SystemTime;
 use unicode_width::UnicodeWidthChar;
 
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const HELP: &str = r#"
+A simplistic terminal text editor for Unix-like systems.
+
+All keymaps as follows.
+
+    Ctrl-Q     : Quit
+    Ctrl-S     : Save to file
+    Ctrl-P     : Move cursor up
+    Ctrl-N     : Move cursor down
+    Ctrl-F     : Move cursor right
+    Ctrl-B     : Move cursor left
+    Ctrl-A     : Move cursor to head of line
+    Ctrl-E     : Move cursor to end of line
+    Ctrl-V     : Next page
+    Alt-V      : Previous page
+    Alt-F      : Move cursor to next word
+    Alt-B      : Move cursor to previous word
+    Alt-<      : Move cursor to top of file
+    Alt->      : Move cursor to bottom of file
+    Ctrl-H     : Delete character
+    Ctrl-D     : Delete next character
+    Ctrl-U     : Delete until head of line
+    Ctrl-K     : Delete until end of line
+    Ctrl-M     : New line
+    Ctrl-G     : Search text
+    Ctrl-L     : Refresh screen
+    Ctrl-?     : Show this help
+    UP         : Move cursor up
+    DOWN       : Move cursor down
+    RIGHT      : Move cursor right
+    LEFT       : Move cursor left
+    PAGE DOWN  : Next page
+    PAGE UP    : Previous page
+    HOME       : Move cursor to head of line
+    END        : Move cursor to end of line
+    DELETE     : Delete next character
+    BACKSPACE  : Delete character
+    ESC        : Refresh screen
+    Ctrl-RIGHT : Move cursor to next word
+    Ctrl-LEFT  : Move cursor to previous word
+    Alt-RIGHT  : Move cursor to end of line
+    Alt-LEFT   : Move cursor to head of line
+"#;
+
 #[derive(PartialEq)]
 enum StatusMessageKind {
     Info,
@@ -160,7 +205,7 @@ impl Screen {
     }
 
     fn draw_welcome_message<W: Write>(&self, mut buf: W) -> io::Result<()> {
-        let msg_buf = format!("Kiro editor -- version {}", crate::VERSION);
+        let msg_buf = format!("Kiro editor -- version {}", VERSION);
         let welcome = self.trim_line(&msg_buf);
         let padding = (self.num_cols - welcome.len()) / 2;
         if padding > 0 {
@@ -344,7 +389,7 @@ impl Screen {
     }
 
     pub fn draw_help(&self) -> io::Result<()> {
-        let help: Vec<_> = crate::HELP
+        let help: Vec<_> = HELP
             .split('\n')
             .skip_while(|s| !s.contains(':'))
             .map(str::trim_start)
