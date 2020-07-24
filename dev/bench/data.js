a98b9b26bcf263600dc1c27bcf0cbdaea79da49a
github-action-benchmark <github@users.noreply.github.com>
2020-07-24 13:32:54 +0000

    add Rust Benchmark (cargo) benchmark result for fd96ec3dc0c771cc08e35819593d7468844b3abc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1595520673166,
+  "lastUpdate": 1595597574914,
   "repoUrl": "https://github.com/rhysd/kiro-editor",
   "entries": {
     "Rust Benchmark": [
@@ -575,6 +575,54 @@ window.BENCHMARK_DATA = {
             "name": "no_term_scroll_up_down_rust_code",
             "value": 8798701,
             "range": "± 795017",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "committer": {
+            "email": "lin90162@yahoo.co.jp",
+            "name": "rhysd",
+            "username": "rhysd"
+          },
+          "distinct": true,
+          "id": "fd96ec3dc0c771cc08e35819593d7468844b3abc",
+          "message": "explain more about undo_count",
+          "timestamp": "2020-07-24T22:27:27+09:00",
+          "tree_id": "79c52bd5a42c57780daa41a4aed5efcbba5d0921",
+          "url": "https://github.com/rhysd/kiro-editor/commit/fd96ec3dc0c771cc08e35819593d7468844b3abc"
+        },
+        "date": 1595597573410,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "no_term_edit_1000_operations_to_10000_chars_plain_text",
+            "value": 21171015,
+            "range": "± 3131308",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "no_term_edit_1000_operations_to_editor_rs",
+            "value": 314083222,
+            "range": "± 22317484",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "no_term_scroll_up_down_plain_text",
+            "value": 1763079,
+            "range": "± 324513",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "no_term_scroll_up_down_rust_code",
+            "value": 9204788,
+            "range": "± 1158660",
             "unit": "ns/iter"
           }
         ]
